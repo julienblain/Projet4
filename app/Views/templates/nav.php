0cdc8f321134b1cef69fb2cad807f9c9d16744b8
julienblain <blain.julien@gmail.com>
2017-12-06 14:47:07 +0100

    correction numerotation chapitre menu

--- app/Views/templates/nav.php
+++ app/Views/templates/nav.php
@@ -2,10 +2,12 @@
    <p>Chapitres précédents </p>
     <nav id="nav-public">
         <ul>
-            <?php foreach($postsTitle as $postTitle): ?>
+            <?php $i = 0;
+            foreach($postsTitle as $postTitle):
+            $i ++; ?>
                 <li class="post-title nav-post-title">
                     <a href="?p=posts.<?= $postTitle->id ?>.selected">
-                        <p><?= $postTitle->id . '.' ?></p>
+                        <p><?=  $i . '.' ?></p>
                         <?=$postTitle->title ?>
                     </a>
                 </li>
