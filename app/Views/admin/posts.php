ca21542ee97403ad1e187309410c5b45d2393e22
julienblain <blain.julien@gmail.com>
2017-12-07 19:10:21 +0100

    creation confirm delete post

--- app/Views/admin/posts.php
+++ app/Views/admin/posts.php
@@ -7,14 +7,14 @@
    <h2>Liste des chapitre : </h2>
     <ul>
         <?php foreach($postsTitle as $postTitle): ?>
-           
+
             <li>
                 <a class="post-listing-title bowlby" href="?p=posts.<?= $postTitle->id ?>.selected">
                     <p><?= $postTitle->id. '.'?></p> <?= $postTitle->title ?>
                 </a>
                 <a class="btn-bowlby" href="?p=posts.<?= $postTitle->id ?>.selected">Lire</a>
                 <a class="btn-bowlby" href="?p=posts.<?= $postTitle->id ?>.update">Modifer</a>
-                <a class="btn-bowlby" href="?p=posts.<?= $postTitle->id ?>.delete">Supprimer</a>
+                <a id='btn-deletePost' class="btn-bowlby" href="?p=posts.<?= $postTitle->id ?>.delete">Supprimer</a>
             </li>
         <?php endforeach; ?>
     </ul>
